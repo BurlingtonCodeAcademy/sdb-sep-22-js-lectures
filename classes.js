30c03390da6a875c0ac8492fe927bb08d482aa69
Paul Niemczyk <mrpndev@gmail.com>
2022-10-13 13:46:43 -0400

    Class Lesson Complete

--- classes.js
+++ classes.js
@@ -58,7 +58,7 @@ class Car {
             this.make = make
             this.model = model
             this.year = year
-             this.transmission = transmission
+            this.transmission = transmission
         } else {
             throw new Error("Constructor is not a string")
         }
@@ -88,3 +88,127 @@ console.log(maserati.greetDriver("Justin"))
 
     Create a method that takes the name of the superhero and checks it against two arrays: one with teamCap and one with teamStark and returns which team the superhero is on as part of Civil War
 */
+
+class Avengers {
+    constructor(name, superpower, age, isDead, keyMovie) {
+        this.name = name
+        this.superpower = superpower
+        this.age = age
+        this.isDead = isDead
+        this.keyMovie = keyMovie
+    }
+
+    introduction(person) {
+        return `Hello ${person}. I'm ${this.name} and my key movie is ${this.keyMovie}`
+    }
+
+    whichTeam() {
+        let teamCap = ["Steve Rogers", "Captain America", "Bucky", "Wanda", "Hawkeye", "Ant Man"]
+        let teamStark = ["Tony Stark", "Vision", "War Machine", "Black Panther", "Black Widow", "Natasha Romanoff"]
+
+        if (teamCap.includes(this.name)) {
+            return `${this.name} is Team Captain`
+        } else if (teamStark.includes(this.name)) {
+            return `${this.name} is Team Stark`
+        } else {
+            return `${this.name} is not in either of our arrays`
+        }
+    }
+}
+
+let ironMan = new Avengers("Tony Stark", "smart & rich", 40, true, "Iron Man")
+
+console.log(ironMan.whichTeam())
+
+// Factory Functions
+
+/* 
+    Function that accepts data and returns new instance of a Class
+*/
+
+class Circle {
+    constructor(radius) {
+        if (typeof radius == "number") {
+            this.radius = radius
+        } else {
+            throw new Error("Radius must be a number")
+        }
+    }
+    
+    circumference() {
+        return (2 * Math.PI * this.radius).toFixed(2)
+    }
+    
+    area() {
+        return (this.radius ** 2 * Math.PI).toFixed(2)
+    }
+}
+
+function circleFromDiameter(diameter) {
+    return new Circle(diameter / 2)
+}
+
+let myCircle = circleFromDiameter(15)
+console.log(myCircle.circumference())
+console.log(myCircle.area())
+
+console.log(myCircle)
+
+// Static Factory Methods
+
+/* 
+Do not require instantiation with a new operator
+*/
+
+class CircleStaticFactoryFx {
+    static createCircleUsingDiameter(diameter) {
+        return new CircleStaticFactoryFx(diameter / 2)
+    }
+    
+    constructor(radius) {
+        if (typeof radius == "number") {
+            this.radius = radius
+        } else {
+            throw new Error("Radius must be a number")
+        }
+    }
+}
+
+let diaCircle = CircleStaticFactoryFx.createCircleUsingDiameter(20)
+
+console.log(diaCircle)
+
+// Inheritance
+
+/* 
+    Extends a parent class with a child class
+*/
+
+class CarOptions extends Car {
+    constructor(
+        make,
+        model,
+        year,
+        transmission,
+        hasLeather,
+        hasComfortPackage,
+        hasSportPackage
+    ) {
+        // allows us access to properties and methods of a parent class
+        super(make, model, year, transmission)
+        this.hasLeather = hasLeather
+        this.hasComfortPackage = hasComfortPackage
+        this.hasSportPackage = hasSportPackage
+    }
+
+    isCPO(currentYear) {
+        let result = currentYear - this.year
+        return result >= 6
+            ? `Your ${this.model} is out of warranty`
+            : `Your warranty is still active`
+    }
+}
+
+let porsche911 = new CarOptions("Porsche", "911", 2017, "PDK", true, false, true)
+
+console.log(porsche911.isCPO())
