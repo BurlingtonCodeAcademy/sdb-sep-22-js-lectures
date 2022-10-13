35aea86bf0ecbc5d11f82d7260ce5745cdf24b35
Paul Niemczyk <mrpndev@gmail.com>
2022-10-13 16:08:43 -0400

    State Machine Lecture Complete

--- state.js
+++ state.js
@@ -25,3 +25,47 @@ function getPoemTitle(authorUserSelect) {
 }
 
 console.log(getPoemTitle("Sylvia Plath"))
+
+let musicianList = (musician) => {
+    let list = {
+        "Illenium": ["Gold", "God Damnit", "Lost"],
+        "ATB": "Ecstasy",
+        "Felix Jaehn": "Call it Love"
+    }
+
+    return list[musician]
+}
+
+console.log(musicianList("Illenium"))
+
+// State Machines
+
+/* 
+    System with finite and defined states.
+    States are just information
+    The info is reliant upon logic in order to transition or be reassigned
+*/
+
+let state = {
+    red: ["green", "yield"],
+    yellow: ["red"],
+    green: ["yellow"],
+    yield: ["red"]
+}
+
+let currentState = "green"
+
+function enterState(newState) {
+    // assigns values of state property that matches current state
+    let validTransitions = state[currentState]
+    // if newState is found in validTransitions
+    if (validTransitions.includes(newState)) {
+        // change the light
+        currentState = newState
+        console.log(currentState)
+    } else {
+        throw new Error(`Invalid state transition from ${currentState} to ${newState}`)
+    }
+}
+
+enterState("red")
